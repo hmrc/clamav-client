40a0ce5489396ef1ab43a9ab12f8620ed68d1736
ralphcollett <ralph.collett@gmail.com>
2016-10-17 15:40:14 +0100

    Create Fake Clam.

---
+++ src/main/scala/uk/gov/hmrc/clamav/fake/FakeClam.scala
@@ -0,0 +1,66 @@
+package uk.gov.hmrc.clamav.fake
+
+import java.io.{BufferedReader, DataOutputStream, IOException, InputStreamReader}
+import java.net.ServerSocket
+
+import play.api.Logger
+import uk.gov.hmrc.clamav.ClamAntiVirus
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class FakeClam(serverSocket: ServerSocket)(implicit executionContext: ExecutionContext) {
+
+  def start(): Future[Unit] = {
+    Future {
+      Logger.debug(s"Fake Clam started on port ${ serverSocket.getLocalPort }")
+      while (! serverSocket.isClosed) {
+        try {
+          val socket = serverSocket.accept()
+          val outputStream = socket.getOutputStream
+          val dataOutputStream = new DataOutputStream(outputStream)
+          val in = new BufferedReader(new InputStreamReader(socket.getInputStream))
+
+          handle(in, dataOutputStream)
+
+          dataOutputStream.close()
+          outputStream.close()
+          socket.close()
+        } catch {
+          case _ :IOException => Logger.debug("IOExeption reading from the socket")
+        }
+      }
+    }
+  }
+
+  private def handle(in: BufferedReader, out: DataOutputStream): Unit = {
+    val received = new String(Iterator.continually(in.read)
+      .takeWhile(_ != 0)
+      .map(_.toByte).toArray)
+
+    received match {
+      case "zINSTREAM" | "" => handle(in, out)
+      case _ =>
+        Logger.debug(s"Responding with ${ClamAntiVirus.okClamAvResponse}")
+        out.writeBytes(ClamAntiVirus.okClamAvResponse)
+        out.flush()
+    }
+  }
+
+  def stop() = {
+    Logger.debug("Stopping Fake Clam")
+    if (! serverSocket.isClosed) serverSocket.close()
+  }
+}
+
+object FakeClam extends App {
+
+  implicit val ec = ExecutionContext.global
+
+  val serverSocket = new ServerSocket(3310)
+  val fakeClam = new FakeClam(serverSocket)
+  try {
+    fakeClam.start().wait()
+  } catch {
+    case _: InterruptedException => fakeClam.stop()
+  }
+}
